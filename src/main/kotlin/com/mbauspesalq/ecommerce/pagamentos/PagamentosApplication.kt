a310c98725c67cc8503c37b946bd0cbb975d5ba0
YanMaciel <araujo.yan@hotmail.com>
2025-03-23 01:23:57 -0300

    hello world

---
+++ src/main/kotlin/com/mbauspesalq/ecommerce/pagamentos/PagamentosApplication.kt
@@ -0,0 +1,11 @@
+package com.mbauspesalq.ecommerce.pagamentos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PagamentosApplication
+
+fun main(args: Array<String>) {
+	runApplication<PagamentosApplication>(*args)
+}
